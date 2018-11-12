0d25ac697143f559fe71459a60cea3b078b73f24
hiro0218 <hiro0218@gmail.com>
2018-11-12 12:51:56 +0900

    change: フォントの読み込みタイミングを変更

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 
 <script>
 import 'normalize.css'
+import 'bulma/css/bulma.css'
 
 import Generator from './components/Generator.vue'
 
@@ -14,23 +15,39 @@ export default {
   name: 'app',
   components: {
     Generator
+  },
+  mounted: function() {
+    this.loadFont();
+  },
+  methods: {
+    loadFont: function() {
+      document.addEventListener("DOMContentLoaded", function() {
+        var link = document.createElement("link");
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href =
+          "https://fonts.googleapis.com/css?family=M+PLUS+1p:400,700&subset=japanese";
+        document.head.appendChild(link);
+      });
+    }
   }
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=M+PLUS+1p:400,700&subset=japanese');
-
 #app {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
-  font-family: 'M PLUS 1p', sans-serif;
+  font-family: 'Noto Sans JP', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+h1 {
+  margin-bottom: 1rem;
 }
 </style>
